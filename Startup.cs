4df35cb07b40b7f7515e98ba5528398f2a41e4a3
poziom <dwrona94@gmail.com>
2020-12-01 22:13:53 +0100

    Added database and Identity

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RSSReader.Data;
 
 namespace RSSReader
 {
@@ -20,7 +22,7 @@ namespace RSSReader
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory

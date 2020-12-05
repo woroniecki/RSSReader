6d9e4921e332b5a4818ea18456812784d571c7d6
poziom <dwrona94@gmail.com>
2020-12-05 17:30:54 +0100

    Authentication Register and login methods

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RSSReader.Dtos;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSReader.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _config;
+
+        //private readonly IEmailSender _emailSender; UnComment if you want to add Email Verification also.
+
+        public AuthController(UserManager<IdentityUser> userManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _config = config;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Wrong data");
+
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (await _userManager.FindByNameAsync(model.Username) != null)
+                return BadRequest("User with this name already exists");
+
+            if (await _userManager.FindByEmailAsync(model.Email) != null)
+                return BadRequest("User with this email already exists");
+
+            var new_user = new IdentityUser
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(new_user, model.Password);
+
+            if (result.Succeeded)
+                return Created("route to set", new_user);
+
+            return BadRequest("Request failed");
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] UserForLoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UsernameOrEmail);
+
+            if (user == null)
+                user = await _userManager.FindByEmailAsync(model.UsernameOrEmail);
+
+            if (user == null)
+                return Unauthorized("Wrong data");
+
+            if (!await _userManager.CheckPasswordAsync(user, model.Password))
+                return Unauthorized("Wrong data");
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]{
+                                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                                new Claim(ClaimTypes.Name, user.Email)
+                    }
+                ),
+                Issuer = "http://localhost:5000/",//TODO
+                Audience = "http://localhost:5000/",//TODO
+                Expires = DateTime.Now.AddHours(3),
+                SigningCredentials = creds,
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                expiration = token.ValidTo,
+                user = user
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,30 @@ namespace RSSReader
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<DataContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequiredUniqueChars = 0;
+
+                // Lockout settings.
+                // options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                // options.Lockout.MaxFailedAccessAttempts = 5;
+                // options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
@@ -57,6 +82,7 @@ namespace RSSReader
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
 
             app.UseSpa(spa =>

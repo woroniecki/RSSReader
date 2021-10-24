2d5a8d8c3a3cbb9d247a1a318dafd28aa131622e
zipsonix <dwrona94@gmail.com>
2021-10-24 17:42:54 +0200

    Added spinner and snackbars to unsubscribing blogs

--- RSSReader/ClientApp/src/components/Blog/UnsubscribeBlogBtn.tsx
+++ RSSReader/ClientApp/src/components/Blog/UnsubscribeBlogBtn.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { useAppDispatch } from 'store/store'
-import { blogsSlice } from 'store/slices'
+import { blogsSlice, snackbarSlice } from 'store/slices'
 import { Blog } from 'api/api.types'
+import SpinnerElement from 'components/Spinner/SpinnerElement'
 
 export interface UnsubscribeBlogBtnProps {
   blog: Blog
@@ -19,9 +20,28 @@ export const UnsubscribeBlogBtn: React.FC<UnsubscribeBlogBtnProps> = props => {
     const promise = await dispatch(blogsSlice.putUnsubscribeBlog(props.blog))
 
     if (blogsSlice.putUnsubscribeBlog.fulfilled.match(promise)) {
+      dispatch(
+        snackbarSlice.actions.setSnackbar({
+          open: true,
+          color: 'success',
+          msg: 'Subscription removed',
+        })
+      )
     } else {
+      dispatch(
+        snackbarSlice.actions.setSnackbar({
+          open: true,
+          color: 'error',
+          msg: 'Removing subscription failed',
+        })
+      )
     }
     setIsInAction(false)
+  }
+
+  function getBtnBody() {
+    if (!isInAction) return <DeleteIcon />
+    return <SpinnerElement size={14} />
   }
 
   return (
@@ -31,7 +51,7 @@ export const UnsubscribeBlogBtn: React.FC<UnsubscribeBlogBtnProps> = props => {
         unsubcribeBlog()
       }}
     >
-      <DeleteIcon />
+      {getBtnBody()}
     </IconButton>
   )
 }

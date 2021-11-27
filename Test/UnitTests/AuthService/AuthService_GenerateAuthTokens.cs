6a0674012b58277361df1339c6b97475b48fbf9f
zipsonix <dwrona94@gmail.com>
2021-11-27 19:00:43 +0100

    Added authservice tests

---
+++ Test/UnitTests/AuthService/AuthService_GenerateAuthTokens.cs
@@ -0,0 +1,56 @@
+﻿using DataLayer.Code;
+using DataLayer.Models;
+using DbAccess.Core;
+using LogicLayer.Helpers;
+using Moq;
+using NUnit.Framework;
+using ServiceLayer.AuthServices;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tests.Fake;
+using Tests.Helpers;
+
+namespace Tests.UnitTests
+{
+    [TestFixture]
+    class AuthService_GenerateAuthTokens
+    {
+        private IAuthService _authService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var config = new FakeConfigGetToken();
+            _authService = new AuthService(config.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public async Task Generate_new_tokens__Tokens_with_correct_expire_date()
+        {
+            //ARRANGE
+            string USER_ID = "USER_ID";
+            ApiUser user = new ApiUser()
+            {
+                Id = USER_ID,
+                UserName = "Name",
+                RefreshTokens = new List<RefreshToken>()
+            };
+            var start_time = DateTime.UtcNow;
+
+            //ACT
+            var tokens = _authService.GenerateAuthTokens(user);
+
+            //ASSERT
+            Assert.That(tokens.AuthToken.Expires, Is.GreaterThan(start_time.From1970()));
+            Assert.That(tokens.RefreshToken.Expires, Is.GreaterThan(start_time.From1970()));
+            Assert.IsFalse(string.IsNullOrEmpty(tokens.AuthToken.Token));
+            Assert.IsFalse(string.IsNullOrEmpty(tokens.RefreshToken.Token));
+        }
+    }
+}
---
+++ Test/UnitTests/AuthService/AuthService_GetUserIdFromToken.cs
@@ -0,0 +1,68 @@
+﻿using DataLayer.Code;
+using DataLayer.Models;
+using DbAccess.Core;
+using Moq;
+using NUnit.Framework;
+using ServiceLayer.AuthServices;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tests.Fake;
+using Tests.Helpers;
+
+namespace Tests.UnitTests
+{
+    [TestFixture]
+    class AuthService_GetUserIdFromToken
+    {
+        private IAuthService _authService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var config = new FakeConfigGetToken();
+            _authService = new AuthService(config.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public async Task Correctly_decode_token_and_finds_user_id__Returns_user_id()
+        {
+            //ARRANGE
+            string USER_ID = "USER_ID";
+            ApiUser user = new ApiUser() { 
+                Id = USER_ID, 
+                UserName = "Name",
+                RefreshTokens = new List<RefreshToken>()
+            };
+            string token = _authService.GenerateAuthTokens(user).AuthToken.Token;
+
+            //ACT
+            string result_id = _authService.GetUserIdFromToken(token);
+
+            //ASSERT
+            Assert.That(result_id, Is.EqualTo(USER_ID));
+        }
+
+        [Test]
+        public async Task Try_to_get_id_from_broken_token__Throw_exception()
+        {
+            //ARRANGE
+            string USER_ID = "USER_ID";
+            ApiUser user = new ApiUser()
+            {
+                Id = USER_ID,
+                UserName = "Name",
+                RefreshTokens = new List<RefreshToken>()
+            };
+            string token = "Invalid.Token.Key";
+
+            //ACT
+            var ex = Assert.Throws<ArgumentException>(() => _authService.GetUserIdFromToken(token));
+        }
+    }
+}

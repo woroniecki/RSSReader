342c2bdf99fa0b2169dc8e4fe7842a68e89fbfde
woroniecki <dwrona94@gmail.com>
2022-01-21 12:45:09 +0100

    Changed expiration time of auth and refresh token

--- ServiceLayer/_const/RssConsts.cs
+++ ServiceLayer/_const/RssConsts.cs
@@ -7,8 +7,8 @@ namespace LogicLayer._const
     {
         public const int POSTS_PER_CALL = 10;
 
-        public static double AUTH_TOKEN_EXPIRES_TIME_S = new TimeSpan(0, 5, 0, 0).TotalSeconds;
-        public static double REFRESH_TOKEN_EXPIRES_TIME_S = AUTH_TOKEN_EXPIRES_TIME_S + new TimeSpan(0, 10, 0, 0).TotalSeconds;
+        public static double AUTH_TOKEN_EXPIRES_TIME_S = new TimeSpan(0, 6, 0, 0).TotalSeconds;
+        public static double REFRESH_TOKEN_EXPIRES_TIME_S = AUTH_TOKEN_EXPIRES_TIME_S + new TimeSpan(30, 0, 0, 0).TotalSeconds;
 
         public static double UPDATE_BLOG_DELAY_S = new TimeSpan(0, 0, 5, 0).TotalSeconds;
     }
